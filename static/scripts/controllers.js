d248697f9d71e42aa0a8f58fc36424af655f6193
Dan Cusher <dscush@gmail.com>
2016-09-10 22:47:46 -0400

    Added radio button list of saved generators for selecting active generator

--- static/scripts/controllers.js
+++ static/scripts/controllers.js
@@ -3,4 +3,10 @@
 /*global angular*/
 angular.module('RcpgApp').controller('RcpgController', ['$scope', '$http', 'NoteNames', 'SavedGenerators', function($scope, $http, NoteNames, SavedGenerators){
     $scope.allNotes = NoteNames.allNotes;
+    $scope.SavedGenerators = SavedGenerators;
+    $scope.newGenerator = function(name) {
+        new SavedGenerators.Generator(name);
+        console.log('fooo');
+        // TODO: set active generator radio button for this generator
+    }
 }]);
